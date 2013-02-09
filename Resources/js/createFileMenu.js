398ddfab85c969a135ec2fab884f72088f9da829
Dahmian Owen <dahmian.owen@gmail.com>
2013-02-09 10:44:37 -0800

    functionalized createFileMenu

--- Resources/js/createFileMenu.js
+++ Resources/js/createFileMenu.js
@@ -3,26 +3,34 @@ define(function(require) {
 
   return function createFileMenu(playlist) {
     var menu = Ti.UI.createMenu();
-    var fileMenu = menu.addItem("File");
-    fileMenu.addItem("Add song to playlist", addSong);
-    fileMenu.addItem("Open playlist", openPlaylist);
-    fileMenu.addItem("Save playlist", savePlaylist);
-    var controlsMenu = menu.addItem("Controls");
-    controlsMenu.addItem("Play", playlist.play);
-    controlsMenu.addItem("Next", playlist.playNext);
-    controlsMenu.addItem("Previous", playlist.playPrevious);
+    addFileMenu(menu);
+    addControlsMenu(menu);
     Ti.UI.getCurrentWindow().setMenu(menu);
 
-    function addSong() {
-      require("addSongToPlaylist")(playlist);
+    function addFileMenu(menu) {
+      var fileMenu = menu.addItem("File");
+      fileMenu.addItem("Add song to playlist", addSong);
+      fileMenu.addItem("Open playlist", openPlaylist);
+      fileMenu.addItem("Save playlist", savePlaylist);
+
+      function addSong() {
+        require("addSongToPlaylist")(playlist);
+      }
+
+      function openPlaylist() {
+        require("openPlaylist")(playlist);
+      }
+
+      function savePlaylist() {
+        require("savePlaylist")(playlist);
+      }
     }
 
-    function openPlaylist() {
-      require("openPlaylist")(playlist);
-    }
-
-    function savePlaylist() {
-      require("savePlaylist")(playlist);
+    function addControlsMenu(menu) {
+      var controlsMenu = menu.addItem("Controls");
+      controlsMenu.addItem("Play", playlist.play);
+      controlsMenu.addItem("Next", playlist.playNext);
+      controlsMenu.addItem("Previous", playlist.playPrevious);
     }
   }
 });
