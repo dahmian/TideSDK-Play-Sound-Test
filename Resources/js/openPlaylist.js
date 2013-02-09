e6c7c308014743bf615345f089d825d9cbdd7a67
Dahmian Owen <dahmian.owen@gmail.com>
2013-02-08 22:57:16 -0800

    playlist import works again

--- Resources/js/openPlaylist.js
+++ Resources/js/openPlaylist.js
@@ -5,6 +5,7 @@ define(function(require) {
     Ti.UI.getCurrentWindow().openFileChooserDialog(fileSelectedCallback, {multiple: false, title: "Open playlist"});
 
     function fileSelectedCallback(filePathsArray) {
+      debugger;
       if (filePathsArray.length === 0) {
         return;
       }
@@ -14,8 +15,12 @@ define(function(require) {
       fileStream.open();
       var line;
       var pathArray = [];
-      while(line = fileStream.readLine().toString()) {
-        pathArray.push(line);
+      while(fileStream.ready()){
+        var line = fileStream.readLine()
+        if (line !== null) {
+          var songPath = line.toString();
+        }
+        pathArray.push(songPath);
       }
       fileStream.close();
       playlist.importSongPaths(pathArray);
